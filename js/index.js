9da3b5d776d2646f06f8fd113e947c8443182bae
7lucky-axolotl228 <7error228@gmail.com>
2026-04-29 02:39:44 +0500

    Add some JS functions to make home page ineractive

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// const text = ["We Build Websites", "That Work", "For Your Business"];
+// let i = 0;
+
+// setInterval(() => {
+//     document.querySelector(".hero-content h1").innerText = text[i];
+//     i = (i + 1) % text.length;
+// }, 2000);
+
+
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".card, .feature-card, .testimonial, .stat").forEach(el => observer.observe(el));
+
+const slider = document.querySelector(".slider");
+const track = document.querySelector(".slide-track");
+
+slider.addEventListener("mouseenter", () => {
+  track.style.animationPlayState = "paused";
+});
+
+slider.addEventListener("mouseleave", () => {
+  track.style.animationPlayState = "running";
+});
